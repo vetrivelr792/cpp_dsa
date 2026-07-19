225244e55ea9603d2a9e6650978f857a888ae52d
vetrivel <vetrivel011@outlook.com>
2026-07-19 11:17:10 +0530

    Add basic array example implementation in C++

---
+++ src/01_arrays/examples/basics.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "array.hpp"
+
+int main() {
+
+  int n;
+
+  std::cout << "Enter the size of array: ";
+  std::cin >> n;
+
+  int A[n];
+
+  A[0] = 100;
+
+  int i = 0;
+  for (int x : A) {
+
+    std::cout << i++ << " " << x << std::endl;
+  }
+
+  return 0;
+}
